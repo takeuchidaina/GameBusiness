b7def267a59b45a66919068fda3b24d65420ad65
kuon8982 <kuon8982@gmail.com>
2020-11-04 15:38:18 +0900

    [update]シーン遷移系のスクリプト追加と編集
    シーン遷移を一つの関数で管理

---
+++ Resouce/Script/StageManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    public static StageManager Instance;
+    public static string nowStageNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //nowStageNum = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StageChange(string _num)
+    {
+        //ステージ番号保存
+        nowStageNum = _num;
+
+        //シーン遷移
+        //SceneChanger.Instance.SceneChange("Stage" + _num);
+        SceneChanger.Instance.SceneChange("Result");
+    }
+}
