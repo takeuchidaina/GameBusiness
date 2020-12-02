5dc1e382bd9053d162464ebb0826e200d55dfe75
tajima <tajima.a.work@gmail.com>
2020-12-02 13:08:35 +0900

    [update]機雷ギミックWIP

--- Resouce/Script/gimmick3.cs
+++ Resouce/Script/gimmick3.cs
@@ -2,44 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class gimmick3 : MonoBehaviour   //反復移動するギミック
+public class gimmick3 : MonoBehaviour   //プレイヤーが近づくと一定時間後爆発するギミック
 {
-
-    Vector3 FirstPosition;  //初期位置(反復する線上の中心点になる)
     [SerializeField]
-    Vector3 MovePosition;   //移動先の座標
-    float Distance;         //2点間の距離、移動フラグの切り替えに使用
+    float timer = 2;
 
-    [SerializeField]
-    float speed;     //移動速度
+    Vector3 FirstPosition;      //初期位置
 
-    int MoveFlg = 1;        //移動フラグ、移動先の座標に向かっていれば1、移動先座標から戻ってくる場合は-1
 
     // Start is called before the first frame update
     void Start()
     {
         FirstPosition = transform.position;
-        Distance = Vector3.Distance(FirstPosition, MovePosition);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //移動処理
-        if(MoveFlg == 1)
-        {
-            transform.position += MovePosition * speed;
-        }
-        else if(MoveFlg == -1)
-        {
-            transform.position -= MovePosition * speed;
-        }
-
-        //もし現在の2点間の距離が、初期の2点間の距離より大きければフラグの切り替え
-        if (Distance < Vector3.Distance(FirstPosition, transform.position))
-        {
-            MoveFlg *= -1;
-        }
 
     }
 
@@ -47,8 +26,19 @@ public class gimmick3 : MonoBehaviour   //反復移動するギミック
     {
         if (collision.gameObject.name == "Player")
         {
-            Debug.Log(collision.gameObject.name);
-            Destroy(collision.gameObject);
+            Debug.Log("DereyStart");
+            Invoke("PlayerDestroy", timer);
+           
         }
     }
+
+    void PlayerDestroy()
+    {
+        //二点間の距離とって、あたり判定の円の半径の内側まだいるならPlayer破壊
+        Destroy(GameObject.Find("Player"));
+
+        //自身の破壊
+
+    }
+
 }
