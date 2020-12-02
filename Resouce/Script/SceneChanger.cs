9dce6901396eb2fcc75261702faaa2b650e56e1b
takeuchidaina <yda.takeuchidaina@gmail.com>
2020-12-02 15:24:16 +0900

    [update]シーンチェンジャーとシーンを更新

--- Resouce/Script/SceneChanger.cs
+++ Resouce/Script/SceneChanger.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Events;
 
 public class SceneChanger : MonoBehaviour
 {
     public static SceneChanger Instance;
+    public AudioClip sound_decision;
+    public AudioClip sound_cancel;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         Instance = this;
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,16 +29,34 @@ public class SceneChanger : MonoBehaviour
 
     public void SceneChange(string _scene)
     {
-        SceneManager.LoadScene(_scene);
+        //se再生
+        audioSource.PlayOneShot(sound_decision);
+
+        //se終了後にシーン変更
+        StartCoroutine(Checking(audioSource,()=> { SceneManager.LoadScene(_scene); }));
     }
 
     public void OnClickEndButton()
     {
-    #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-    #else
-        Application.Quit();
-    #endif
+#if UNITY_EDITOR
+        //se再生
+        audioSource.PlayOneShot(sound_cancel);
+
+        //se終了後にシーン変更
+        StartCoroutine(Checking(audioSource, () => 
+        { UnityEditor.EditorApplication.isPlaying = false; }
+        ));
+
+#else
+        //se再生
+        audioSource.PlayOneShot(sound_cancel);
+
+        //se終了後にシーン変更
+        StartCoroutine(Checking(audioSource, () => 
+        { Application.Quit(); }
+        ));
+        
+#endif
     }
 
     public void OnClickTryAgain()
@@ -58,5 +81,20 @@ public class SceneChanger : MonoBehaviour
     {
         StageManager.nowStageNum = "1";
         SceneChange("Result");
+    }
+
+    //サウンドが終了しているか確認する
+    //https://qiita.com/TakashiHamada/items/a5ff15ca7e63c06bfb23
+    private IEnumerator Checking(AudioSource audio, UnityAction callback)
+    {
+        while (true)
+        {
+            yield return new WaitForFixedUpdate();
+            if (!audio.isPlaying)
+            {
+                callback();
+                break;
+            }
+        }
     }
 }
