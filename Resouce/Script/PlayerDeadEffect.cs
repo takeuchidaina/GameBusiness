78dff315eefb6e5c685413f09b804a4448a1dd21
taki <takagiyda@gmail.com>
2020-12-09 14:58:50 +0900

    [update]プレイヤーの挙動修正
    それっぽくなりました

--- Resouce/Script/PlayerDeadEffect.cs
+++ Resouce/Script/PlayerDeadEffect.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class PlayerDeadEffect : MonoBehaviour
 {
     public GameObject particleObject;
+    public AudioSource audio;
+
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Object") 
+        if (collision.gameObject.tag == "Wall") 
         {
             Instantiate(particleObject, this.transform.position, Quaternion.identity); //パーティクル用ゲームオブジェクト生成
             Destroy(this.gameObject); //衝突したゲームオブジェクトを削除
+            audio.PlayOneShot(audio.clip);
         }
     }
 }
