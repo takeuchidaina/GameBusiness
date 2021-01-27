730f08672f89bdf23e4df8311dcd51f2d57e3a9a
takeuchidaina <yda.takeuchidaina@gmail.com>
2021-01-27 16:36:24 +0900

    [add]タイマーの追加

---
+++ Resouce/Script/TimeATK.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeATK : MonoBehaviour
+{
+    public static double timer;
+    GameObject timeObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0.0;
+        timeObj = GameObject.Find("Time");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        timeObj.GetComponent<Text>().text = timer.ToString("F2");
+    }
+}
